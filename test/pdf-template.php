6f2aa6809c88d6dcd2ecac64a343db187975431a
Yancy Suarez <suarez.yancy@gmail.com>
2020-10-11 02:58:27 +0800

    Added: PDF Generation

---
+++ test/pdf-template.php
@@ -0,0 +1,44 @@
+<?php 
+  // Set Default System Timezone
+  date_default_timezone_set("Asia/Manila");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../core/css/pdf-template-style.css">
+</head>
+<body>
+    <div class="row header">
+        <div class="col-12 logo-container text-center">
+            <img src="../core/img/ollh-logo.gif" alt="ollh-logo">
+        </div>
+        <div class="col-12 header-text text-center">
+            Our Lady of Lourdes Hospital <br>
+            OLLH Employee eTriage <br>
+            Gate Pass
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 text-right">
+            Saturday, October 10, 2020
+        </div>
+    </div>
+
+    <div class="row text-center header">
+        <div class="col-12 header-text">eTriage Health Declaration Status : </div>
+        <div class="col-12">
+            <h2 class='uppercase health-status'>Without any symptoms</h2>
+        </div>
+        <div class="col-12 uppercase"><b>0000-1751 &minus; Yancy L. Suarez</b></div>
+        <div class="col-12">ITS Department</div>
+        <div class="col-12">Office of the President</div>
+    </div>
+</div>
+</body>
+</html>
